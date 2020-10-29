0ee63666fc83d9d443c3b534cdfd4f2c46733590
Uddip Mitra <uddipmitra@gmail.com>
2020-10-29 02:24:39 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// start by typing fm and intellisense will show fmmain which when selected
+// will populate the func main bare structure.
+func main() {
+	fmt.Println(">>>>>+++++++ begin main +++++++<<<<<")
+	primitives()
+
+	collections()
+	fmt.Println(">>>>>++++++++ end main ++++++++<<<<<")
+}
