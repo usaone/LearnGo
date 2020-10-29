6a636bb5ecae098b4a9120d32d0c9fe305e1228d
Uddip Mitra <uddipmitra@gmail.com>
2020-10-29 03:14:58 -0400

    Add gitignore and readme

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/pluralsight/webservice/models"
 )
 
 // start by typing fm and intellisense will show fmmain which when selected
 // will populate the func main bare structure.
 func main() {
 	fmt.Println(">>>>>+++++++ begin main +++++++<<<<<")
+	u := models.User{
+		ID:        2,
+		FirstName: "Tricia",
+		LastName:  "McMillan",
+	}
+	fmt.Println(u)
 
 	fmt.Println(">>>>>++++++++ end main ++++++++<<<<<")
 }
