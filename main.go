faf2d8ace548780199a1f49422aeac81a4bf1cbb
Uddip Mitra <uddipmitra@gmail.com>
2020-10-29 22:03:35 -0400

    Use function with argumenta and return type

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/pluralsight/webservice/models"
 )
 
 // start by typing fm and intellisense will show fmmain which when selected
 // will populate the func main bare structure.
 func main() {
 	fmt.Println(">>>>>+++++++ begin main +++++++<<<<<")
-	u := models.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
-	fmt.Println(u)
+
+	port := 3000
+	isStarted := startWebServer(port, 5)
+	fmt.Println(isStarted)
 
 	fmt.Println(">>>>>++++++++ end main ++++++++<<<<<")
+}
+
+func startWebServer(port, numberOfRetries int) bool {
+	fmt.Println("Starting web server...")
+	// do startup things
+	fmt.Println("Server started : Listening on port", port)
+	fmt.Println("Number of tries", numberOfRetries)
+	return true
 }
